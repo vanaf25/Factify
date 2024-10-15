b9ca7fb8a2aa38b9ed2ced55de54ef757e8801e8
vanaf25 <vanayfefilov777@gmail.com>
2024-10-15 17:27:16 +0200

    initial commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://fast-check-b32d4h6ee-vanaf25s-projects.vercel.app/',
+    baseURL: 'https://fast-check-psi.vercel.app/',
     headers:{
         Authorization:`Bearer ${localStorage.getItem("token")}`,
     }
