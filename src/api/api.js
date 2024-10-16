f74d352d3e633626b074d14c578ce340ce987b41
vanaf25 <vanayfefilov777@gmail.com>
2024-10-16 09:59:25 +0200

    initial commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+const BASE_URL=`http://localhost:5000/`;
+const REMOTE_URL=`https://fast-check-psi.vercel.app/`
 const instance = axios.create({
-    baseURL: 'https://fast-check-psi.vercel.app/',
+    baseURL: REMOTE_URL,
     headers:{
         Authorization:`Bearer ${localStorage.getItem("token")}`,
     }
