f3f0c853f599bc900897326fea24762a129eddad
vanaf25 <vanayfefilov777@gmail.com>
2024-10-18 16:04:31 +0200

    added gif loader

--- src/components/global/Loader/Loader.jsx
+++ src/components/global/Loader/Loader.jsx
@@ -1,8 +1,8 @@
-import LoaderAnimated from './../../../assets/LoaderAnimated.gif'
+import LoaderAnimated2 from './../../../assets/Loader2.gif'
 const LoaderComponent = () => {
     return (
         <div>
-            <img style={{height:250}} className={"height-[150px]"} src={LoaderAnimated} height={250} alt={"Loading"}/>
+            <img style={{height:250}} className={"height-[150px]"} src={LoaderAnimated2} height={250} alt={"Loading"}/>
         </div>
     );
 };
