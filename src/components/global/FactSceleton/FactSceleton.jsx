74cd3e9b545d75b8f364048ef1f9d37bbcf800a8
vanaf25 <vanayfefilov777@gmail.com>
2024-10-19 18:18:53 +0200

    added ability to change password

--- src/components/global/FactSceleton/FactSceleton.jsx
+++ src/components/global/FactSceleton/FactSceleton.jsx
@@ -1,16 +1,18 @@
 const FactSceleton = ({isHistory}) => {
     return (
-        <div
-            className="bg-cardBg p-6 w-[400px] rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex flex-col justify-between animate-pulse">
-            <div className="space-y-2">
-                <div className="w-3/4 h-6 bg-gray-300 rounded-md"></div>
-                <div className="w-1/4 h-4 bg-gray-300 rounded-md"></div>
-            </div>
-            <div className="flex justify-between mt-4">
-                <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
-                {isHistory ? <div className="flex space-x-2">
-                    <div className="w-8 h-8 bg-gray-300 rounded-md"></div>
-                </div>:""}
+        <div className={" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"}>
+            <div
+                className="bg-cardBg p-6  rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex flex-col justify-between animate-pulse">
+                <div className="space-y-2">
+                    <div className="w-3/4 h-6 bg-gray-300 rounded-md"></div>
+                    <div className="w-1/4 h-4 bg-gray-300 rounded-md"></div>
+                </div>
+                <div className="flex justify-between mt-4">
+                    <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
+                    {isHistory ? <div className="flex space-x-2">
+                        <div className="w-8 h-8 bg-gray-300 rounded-md"></div>
+                    </div> : ""}
+                </div>
             </div>
         </div>
     );
